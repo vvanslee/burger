f2ee6ed0628c032b610d25d8b3a10678b65691ec
vvanslee <vvanslee@gmail.com>
2017-08-20 21:28:22 -0500

    updated seed deafult

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,7 +3,7 @@ use burgers_db;
 CREATE TABLE burgers (
 id INTEGER AUTO_INCREMENT,
 burger_name VARCHAR(255) NOT NULL,
-devoured BOOLEAN NOT NULL,
+devoured BOOLEAN DEFAULT false,
 date TIMESTAMP NOT NULL,
 PRIMARY KEY (id)
 );
