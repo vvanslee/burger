79302ad038a199dedb0357f3402e0f81da650d5c
vvanslee <vvanslee@gmail.com>
2017-08-20 20:01:57 -0500

    updated config

--- config/connection.js
+++ config/connection.js
@@ -12,12 +12,5 @@ if (process.env.JAWSDB_URL) {
   });
 };
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+connecection.connect();
 module.exports = connection;
